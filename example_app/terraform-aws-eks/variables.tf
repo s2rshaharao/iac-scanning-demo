a543b9f572cc8137dab31db08a74b07c3870e8cb
Rushikesh Shaharao <rShaharao@MacBook-Pro.local>
2025-08-13 13:01:32 +0530

    Fix EKS cluster control plane logging - enable all log types for security compliance

--- example_app/terraform-aws-eks/variables.tf
+++ example_app/terraform-aws-eks/variables.tf
@@ -41,7 +41,7 @@ variable "kubernetes_version" {
 variable "enabled_log_types" {
   description = "A list of the desired control plane logs to enable. For more information, see Amazon EKS Control Plane Logging documentation (https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html)"
   type        = list(string)
-  default     = ["audit", "api", "authenticator"]
+  default     = ["audit", "api", "authenticator", "controllerManager", "scheduler"]
 }
 
 variable "force_update_version" {
